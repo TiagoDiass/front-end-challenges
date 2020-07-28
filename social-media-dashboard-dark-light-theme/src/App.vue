250f5361bb9ad85f9f51dc21da4c41d858875917
TiagoDiass <tiago.costadiasss@gmail.com>
2020-07-27 21:47:24 -0300

    Creating vue project

---
+++ social-media-dashboard-dark-light-theme/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>
+      Manoooo
+    </h1>
+
+    <h2>Eu precisooo</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+};
+</script>
