aeb52513a3b45d2367cc243b2aa7304cc9165984
TiagoDiass <tiago.costadiasss@gmail.com>
2020-07-28 22:56:04 -0300

    Starting subtitle

--- social-media-dashboard-dark-light-theme/src/App.vue
+++ social-media-dashboard-dark-light-theme/src/App.vue
@@ -3,18 +3,22 @@
     <DashboardHeader />
 
     <OverviewCards />
+
+    <StatisticsArea />
   </div>
 </template>
 
 <script>
 import DashboardHeader from './components/DashboardHeader';
 import OverviewCards from './components/OverviewCards';
+import StatisticsArea from './components/StatisticsArea';
 
 export default {
   name: 'App',
   components: {
     DashboardHeader,
     OverviewCards,
+    StatisticsArea,
   },
 };
 </script>
---
+++ social-media-dashboard-dark-light-theme/src/components/StatisticsArea.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="statistics">
+    <h2 class="statistics-title">Overview - Today</h2>
+
+    <div class="stats-cards-grid">
+      <StatisticCard v-for="(stats, index) in statistics" :key="index" :data="stats" />
+    </div>
+  </section>
+</template>
+
+<script>
+import StatisticCard from './StatisticsCard.vue';
+
+export default {
+  components: {
+    StatisticCard,
+  },
+
+  computed: {
+    statistics() {
+      return [{}];
+    },
+  },
+};
+</script>
