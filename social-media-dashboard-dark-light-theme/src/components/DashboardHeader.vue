27c1d56147badfe1d17f271f3b9585d346f508b9
TiagoDiass <tiago.costadiasss@gmail.com>
2020-07-29 16:23:01 -0300

    Estilização consertada, iniciando dark the,e

--- social-media-dashboard-dark-light-theme/src/components/DashboardHeader.vue
+++ social-media-dashboard-dark-light-theme/src/components/DashboardHeader.vue
@@ -6,12 +6,26 @@
 
     <div class="header-small-row">
       <h4>Total Followers: 23,004</h4>
+
+      <div class="theme-switcher">
+        <h5>Dark Mode</h5>
+        <input type="checkbox" id="checkbox" class="checkbox" />
+        <label for="checkbox" class="label" @click="changeTheme">
+          <div class="ball"></div>
+        </label>
+      </div>
     </div>
   </header>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    changeTheme() {
+      document.querySelector('#app').classList.toggle('dark-mode');
+    },
+  },
+};
 </script>
 
 <style></style>
