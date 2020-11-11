651daa8f0c830faf4be6d98da5492b4fddbe4fe6
TiagoDiass <tiago.costadiasss@gmail.com>
2020-11-11 13:37:06 -0300

    base project done

---
+++ faq-card/questionsAndAnswers.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    question: 'How many team members can I invite?',
+    answer: `You can invite up to 2 additional users on the Free plan. There is no limit on 
+    team members for the Premium plan.`,
+  },
+  {
+    question: 'What is the maximum file upload size?',
+    answer: `No more than 2GB. All files in your account must fit your allotted storage space.`,
+  },
+  {
+    question: 'How do I reset my password?',
+    answer: `Click “Forgot password” from the login page or “Change password” from your profile page.
+    A reset link will be emailed to you.`,
+  },
+  {
+    question: 'Can I cancel my subscription?',
+    answer: `Yes! Send us a message and we’ll process your request no questions asked.`,
+  },
+  {
+    question: 'Do you provide additional support?',
+    answer: `Chat and email support is available 24/7. Phone lines are open during normal business hours.`,
+  },
+];
