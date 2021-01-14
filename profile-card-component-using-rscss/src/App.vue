39eaba168fa311c0fd43229fe7a6a430c118088c
TiagoDiass <tiago.costadiasss@gmail.com>
2021-01-14 10:23:21 -0300

    starting new project to test rscss

---
+++ profile-card-component-using-rscss/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <ProfileCard :profile="profile" />
+  </div>
+</template>
+
+<script>
+import ProfileCard from './components/ProfileCard/ProfileCard.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    ProfileCard,
+  },
+
+  data: () => ({
+    profile: {
+      name: 'Victor Crest',
+      imagePath: 'victor-image.jpg',
+      coverImagePath: 'coverPattern.svg',
+      age: 26,
+      city: 'London',
+      statistics: {
+        followersAmount: '20K',
+        likesAmount: '100K',
+        photosAmount: '1.4K',
+      },
+    },
+  }),
+};
+</script>
+
+<style lang="scss">
+@import './assets/styles/main';
+</style>
---
+++ profile-card-component-using-rscss/src/components/ProfileCard/ProfileCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="profile-card">
+    <img
+      :src="profile.coverImagePath"
+      :alt="`${profile.name}'s cover photo`"
+      class="coverimage"
+    />
+    <main class="data"></main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'profile-card',
+
+  computed: {
+    prof() {
+      return JSON.stringify(this.profile, null, 4);
+    },
+  },
+
+  props: {
+    profile: {
+      type: Object,
+      required: true,
+      description: 'The profile that will appear in this card',
+    },
+  },
+};
+</script>
+
+<style src="./ProfileCard.styles.scss" lang="scss" scoped></style>
